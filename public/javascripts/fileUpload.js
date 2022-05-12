8ba823b1b884ee04494bec2be49cff20eb2bec9f
FantasticBoyZ <khoidragonslayer@gmail.com>
2022-05-12 10:55:19 +0700

    Finished Project

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,12 +1,30 @@
-FilePond.registerPlugin(
-  FilePondPluginImagePreview,
-  FilePondPluginImageResize,
-  FilePondPluginFileEncode,
-)
+const rootStyles = window.getComputedStyle(document.documentElement)
 
-FilePond.setOptions({
-  stylePanelAspectRatio: 150 / 100,
-  imageResizeTargetWidth: 100,
-  imageResizeTargetHeight: 150,
-})
-FilePond.parse(document.body);
+if(rootStyles.getPropertyValue('--book-cover-width-large') != null && 
+rootStyles.getPropertyValue('--book-cover-width-large') != '') {
+  ready()
+}else {
+  document.getElementById('main-css').addEventListener('load', ready)
+}
+
+function ready(){
+  const coverWidth = parseFloat(
+    rootStyles.getPropertyValue
+    ('--book-cover-width-large'))
+  const coveAspectRatio = parseFloat(
+    rootStyles.getPropertyValue
+    ('--book-cover-aspect-ratio'))
+  const coverHeight = coverWidth / coveAspectRatio
+  FilePond.registerPlugin(
+    FilePondPluginImagePreview,
+    FilePondPluginImageResize,
+    FilePondPluginFileEncode,
+  )
+  
+  FilePond.setOptions({
+    stylePanelAspectRatio: 1 / coveAspectRatio,
+    imageResizeTargetWidth: coverWidth,
+    imageResizeTargetHeight: coverHeight,
+  })
+  FilePond.parse(document.body);
+}
